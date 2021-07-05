f327cc5dd396048609f2b883c996edd579bba5cc
Adalkis Peña <adalkis2000@gmail.com>
2021-07-04 22:41:36 -0400

    uploading music player project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <musicplayer/>
+     <link rel="stylesheet" 
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" 
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" 
+        crossorigin="anonymous">
+    
+  </div>
+</template>
+
+<script>
+
+import musicplayer from './components/musicplayer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    musicplayer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+div#app {
+    display: flex;
+    justify-content: center;
+}
+</style>
